5ad9d809ae4c586590ca4d7055fa4a646f42b697
saurabh kumar <saurabhkaswa04@gmail.com>
2026-04-21 17:27:15 +0530

    URL error solved

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -3,8 +3,10 @@ import { API_BASE_URL } from "./config";
 
 export const AUTH_TOKEN_KEY = "flashcard_maker_token";
 
+
+
 const api = axios.create({
-  baseURL: API_BASE_URL + "api",
+  baseURL: `${API_BASE_URL}/api`, 
 });
 
 api.interceptors.request.use((config) => {
